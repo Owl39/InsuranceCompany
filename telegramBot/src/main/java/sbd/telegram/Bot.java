285e50b05242c5c81ac85c80c8077529f05336f2
Owl39 <owl39.sv@gmail.com>
2023-12-15 21:27:12 +0200

    added buttonsPopUp() refactored printText()

--- telegramBot/src/main/java/sbd/telegram/Bot.java
+++ telegramBot/src/main/java/sbd/telegram/Bot.java
@@ -2,11 +2,18 @@ package sbd.telegram;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -27,18 +34,19 @@ public class Bot extends TelegramLongPollingBot {
         return "6735510509:AAGEk_vrWYGF-o5FcyAzV3pJIsdLNN5V3mg";
     }
 
+    public SendMessage printText(String text) {
+        message.setText(text);
+        return message;
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
-//        var message = update.getMessage();
-//        System.out.println(message.getText());
-
         log.debug("Receive new Update. updateID: " + update.getUpdateId());
         Long chatId = update.getMessage().getChatId();
 
-
         message.setChatId(String.valueOf(chatId));
         String inputText = update.getMessage().getText();
-        if (inputText.startsWith("/start")) {
+        if (inputText.equals("/start")) {
             log.debug(inputText);
             createClient(chatId);
         }
@@ -49,23 +57,50 @@ public class Bot extends TelegramLongPollingBot {
 //        TODO основная зона отвецтвенности бота, после ввода укоманды /key
 //         будет выведено кнопки с вариантами задач
 //         https://habr.com/ru/articles/746370/
-        if (inputText.equals("/key")){
+//        String data = update.getCallbackQuery().getData();
+        if (inputText.equals("/key")) {
             log.debug(inputText);
-            textException("Дальше кнопочки сделать");
+            try {
+                execute(buttonsPopUp());
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+//            if (data.equals("Кнопка1")) {
+//                try {
+//                    execute(printText("111"));
+//                } catch (TelegramApiException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
         }
     }
 
-    public void textException(String text){
-        message.setText(text);
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
+    //    TODO Вынести в отдельный класс наверное
+    @SneakyThrows
     public void createClient(Long chatId) {
-        textException ("Вас приведствует Insurance Company Bot. Ваш персональный id:" + chatId+ "\nДля начала работы введите /key");
-//        TODO сделать запрос в бд, чтобы внести всю информацию сразу
+        execute(printText("Вас приведствует Insurance Company Bot. Ваш персональный id:" + chatId + "\nДля начала работы введите /key"));
+        //        TODO сделать запрос в бд, чтобы внести всю информацию сразу
+    }
+
+    @SneakyThrows
+    public SendMessage buttonsPopUp() {
+        printText("Выберете варианты работы");
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+
+        List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Кнопка1");
+//        button1.setUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=0s");
+        button1.setCallbackData("Кнопка1");
+        rowInline1.add(button1);
+
+        rowsInline.add(rowInline1);
+
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+        return message;
     }
 }
